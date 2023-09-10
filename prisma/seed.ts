317ce800443868e989cc5e18f324b1000717de03
Uhyeon Park <98dngus@gmail.com>
2023-09-10 22:16:44 +0900

    fix: fix seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,33 @@
 import { PrismaClient } from "@prisma/client";
-import { fetchJudgeApi } from "@/utils/judgeServerUtils";
-import { ILanguage } from "@/types/common";
 
+const JUDGE_API_URL = process.env.JUDGE_HOST || "";
 const prisma = new PrismaClient();
 
-async function main() {
-  const availableLanguages = (
-    await fetchJudgeApi<any>("/languages", {
-      next: {
-        revalidate: 60 * 60 * 24,
-      },
-    })
-  ).map((lang: Omit<ILanguage, "monacoLanguage">) => {
-    return {
-      ...lang,
-      monacoLanguage: lang.name
-        .split(" (")[0]
-        .toLowerCase()
-        .replaceAll("+", "p")
-        .replaceAll("#", "sharp"),
-    };
+const JUDGE_HEADER = {
+  "Content-Type": "application/json",
+  "X-Auth-Token": process.env.JUDGE_SECRET || "",
+};
+
+const fetchJudgeApi = async <T>(url: string): Promise<T> => {
+  const response = await fetch(JUDGE_API_URL + url, {
+    headers: JUDGE_HEADER,
   });
+  return response.json();
+};
+
+async function main() {
+  const availableLanguages = (await fetchJudgeApi<any>("/languages")).map(
+    (lang: Omit<any, "monacoLanguage">) => {
+      return {
+        ...lang,
+        monacoLanguage: lang.name
+          .split(" (")[0]
+          .toLowerCase()
+          .replaceAll("+", "p")
+          .replaceAll("#", "sharp"),
+      };
+    },
+  );
 
   const languages = await prisma.language.createMany({
     data: availableLanguages,
